fc06fcda19baab26321a1f178b715a75966d4bf8
Adam Cox <Adam.Cox@gov.ab.ca>
2024-11-20 14:09:27 -0500

    refactor & imcr. nottom margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,14 @@ import { OverlayContainer } from '@angular/cdk/overlay';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
 
+interface StoryModel {
+  title: string;
+  subtitle: string;
+  overview: string;
+  benefits: string[];
+  approach: string[];
+  summary: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,6 +27,78 @@ export class AppComponent {
   title = 'Vorba';
   //theme = 'dark';
   isLightTheme = false;
+
+  stories = [
+    {
+      title: 'Success Story 1',
+      subtitle: 'Modernize System',
+      overview: 'Our customer has a critical part of their main business workflow dependent on a system that was\
+        built over 10 years ago.  While they are able to maintain this system, the risk of doing so increases.\
+        It is also a barrier to them developing their service offering.  Due to the complexity and technical\
+        debt of the current system, our customer\'s previous attempts to modernize it have failed.',
+      benefits: [
+        'increase supportability of the system',
+        'decrease cost to support',
+        'able to extend feature-set while maintaining level of support cost',
+      ],
+      approach: [
+        'collect and analyse the existing system',
+        'collect and analyse the documented (and undocumented) SOPs',
+        'document analysis and scope of work',
+      ],
+      summary: 'Our disciplined approach and no-problem attitude provide the foundation for delivering the success\
+        to our customer.  We bring expertise across all problem domains including the personel.  True story,\
+        when we engaged with our client, they required to bring their SME from retirement in order to help\
+        assure our work.  When we originally interviewed the client, we were informed of the long and troubled\
+        history of this work.  We offered our client\'s person our guarantee and worked professionally and delivered\
+        to them the success that they were banking on.'
+    }, {
+      title: 'Success Story 2',
+      subtitle: 'Extend System',
+      overview: 'Our customer wishes to realize a new line of revenue that depends on extending their current systems.\
+        They have their requirements, and have attempted on a few occassions to produce a feature from their backlog\
+        that will realize them this new line of revenue.  However, the difficult nature of this feature (a rate calculator)\
+        has been a barrier to its successful delivery.',
+      benefits: [
+        'deliver supportable system feature',
+        'maintain sensitivity to current working environment',
+        'deliver new line of revenue, and supportable feature',
+      ],
+      approach: [
+        'gather and analyse the existing requirements',
+        'analyse the documented (and undocumented) SOPs',
+        'document analysis and scope of work',
+        'interview and train user group members'
+      ],
+      summary: 'Our disciplined approach and no-problem attitude provide the foundation for delivering the success\
+        to our customer.  We bring expertise across all problem domains including the personel.  True story,\
+        the requirements had critical gaps that would required closure as a precondition of successful delivery\
+        of this feature.  During our agile approach, we identified this gaps, coached our customer through to\
+        their resolve, and went on to deliver a successful feature both affordable and maintainable.  Our customer\
+        reported ROI within the first year of operation.',
+    }, {
+      title: 'Success Story 3',
+      subtitle: 'Proof of Concept',
+      overview: 'Our customer needs to prove a concept requiring MVP-style deliver. Our agile approach will provide\
+        them the opportunity to augment their existing team, and show-case their technical prowess in the form\
+        of new features that are heavilty reactive and UX focused.',
+      benefits: [
+        'work with existing, agile team structure',
+        'maintain sensitivity to current working environment',
+        'show-case new opportunities',
+      ],
+      approach: [
+        'gather and analyse the existing team SOPs',
+        'analyse business area knowledge',
+        'learn from, and coach and train teammates',
+      ],
+      summary: 'Our disciplined approach and no-problem attitude provide the foundation for delivering the success\
+        to our customer.  We bring expertise across all problem domains including the personel.  Our client\
+        had their budgets locked in and time-lines narrowing fast.  We were able to increase bandwidth and\
+        provide their work-force with fresh perspectives and new and innovative ways to reduce time and\
+        increase engagement.',
+    }
+  ] as StoryModel[];
 
   overlayContainer = inject(OverlayContainer);
 
