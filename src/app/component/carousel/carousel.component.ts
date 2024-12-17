701abe4e7fe675eff09a1c2c2a543ffc69c67575
Adam Cox <Adam.Cox@gov.ab.ca>
2024-12-16 22:37:57 -0500

    refactor carousel and add some content

--- src/app/component/carousel/carousel.component.ts
+++ src/app/component/carousel/carousel.component.ts
@@ -1,6 +1,13 @@
-import { AfterViewInit, Component, ContentChildren, OnInit, QueryList, TemplateRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ContentChildren, CUSTOM_ELEMENTS_SCHEMA, ElementRef, Input, OnInit, QueryList, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+
+import SwiperCore from 'swiper';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { AutoplayOptions, SwiperOptions } from 'swiper/types';
+
+registerSwiperElements();
 
 @Component({
   selector: 'app-carousel',
@@ -8,46 +15,42 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [CommonModule, MatIconModule],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class CarouselComponent implements OnInit, AfterViewInit {
-  @ContentChildren(TemplateRef) slides!: QueryList<TemplateRef<any>>;
-  validSlides: TemplateRef<any>[] = [];
-  activeSlideIndex: number = 1;
+export class CarouselComponent implements AfterViewInit {
+  @ContentChildren('slide') slides!: QueryList<TemplateRef<any>>;
+  @Input() autoplayDelayMs = 60000;
+  @ViewChild('swiper') swiperRef: ElementRef<HTMLElement & { swiper?: SwiperCore } & { initialize: () => void }> | undefined;
 
-  constructor() {
-  }
-
-  ngAfterContentInit(): void {
-    // Convert QueryList to array and exclude first and last templates
-    const allSlides = this.slides.toArray();
-    console.log('ngAfterContentInit allSlides:', allSlides); // Debug the filtered slides
-    this.validSlides = allSlides.slice(1, allSlides.length); // Remove first and last templates
-    console.log('ngAfterContentInit validSlides:', this.validSlides); // Debug the filtered slides
-  }
+  swiper?: SwiperCore;
 
   ngAfterViewInit(): void {
-    //throw new Error('Method not implemented.');
-    //console.log('ngAfterViewInit slides:', this.slides);
-  }
-
-  ngOnInit(): void {
-    //throw new Error('Method not implemented.');
-    console.log('ngOnInit slides:', this.slides);
-  }
-
-  prevSlide(): void {
-    if (this.activeSlideIndex > 0) {
-      this.activeSlideIndex--;
-    }
-  }
-
-  nextSlide(): void {
-    if (this.activeSlideIndex < this.slides.length - 1) {
-      this.activeSlideIndex++;
-    }
-  }
-
-  goToSlide(index: number): void {
-    this.activeSlideIndex = index;
+    const progressCircle = document.querySelector<HTMLElement>(".autoplay-progress svg");
+    const progressContent = document.querySelector<HTMLElement>(".autoplay-progress span");
+    const swiperEl = Object.assign(this.swiperRef!.nativeElement, {
+      modules: [ Autoplay, Navigation, Pagination ],
+      autoplay: {
+        delay: this.autoplayDelayMs,
+        disableOnInteraction: false,
+      },
+      loop: true, // not having this here will cause next page button to skip to last page?
+      navigation: true,
+      pagination: {
+        clickable: true,
+      },
+      on: {
+        init: () => {
+          console.log('Swiper initialized');
+        },
+        // https://codesandbox.io/p/sandbox/9q8l47?file=%2Findex.html%3A129%2C46-131%2C70
+        autoplayTimeLeft: (s: any, time: any, progress: any) => {
+          //console.log('Swiper autoplay time left:', s, time, progress);
+          progressCircle!.style.setProperty("--progress", (1 - progress).toString());
+          progressContent!.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
+    swiperEl.initialize();
+    this.swiper = this.swiperRef!.nativeElement.swiper;
   }
 }
