7611dab5f4a93a879d27ef9fc7240f1d0c7b0638
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-07-24 17:23:35 -0400

    also make the default (local) environ file have same urls (troubleshooting)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000/api'  // Adjust this to match your backend URL
-  backendApiUrl: 'http://localhost:3000/api',
-  fileServiceApiUrl: 'http://localhost:3000'
+  backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
+  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net/api'
 };
