d735c124af1b2eff44dfb8d7ee0b4f60fd692295
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-07-24 17:28:31 -0400

    change file-service envion path (remove /api)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000/api'  // Adjust this to match your backend URL
   backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
-  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net/api'
+  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net'
 };
