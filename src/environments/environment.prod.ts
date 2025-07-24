8716b68424ebfb375be61fd6a75e327e8488ccc0
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-07-24 17:12:03 -0400

    update prod environment urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.yourdomain.com/api',  // DEPRECATED
-  backendApiUrl: 'http://localhost:3000/api',
-  fileServiceApiUrl: 'http://localhost:3000'
+  backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
+  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net/api'
 };
