c78c1cb2698742f18f85ec81250835db8c7917df
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-07-27 00:18:50 -0400

    rFIX: emove /api from fileServiceApiUrl, for some unknown reason the latest generated file-service-api ammends the /api path -- NEEDS FURTHER REVIEW

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.yourdomain.com/api',  // DEPRECATED
   backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
-  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net/api'
+  fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net'
 };
