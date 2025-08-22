433c12d93546f3ca936d3217ba1efd166cf2a03b
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-08-21 20:46:25 -0400

    ngx-extended-pdf-viewer

--- src/app/component/page/file-upload-page/_component/file-report-page/file-report-page.component.ts
+++ src/app/component/page/file-upload-page/_component/file-report-page/file-report-page.component.ts
@@ -1,25 +1,32 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { ExamplePdfViewerComponent } from "src/app/component/example-pdf-viewer/example-pdf-viewer.component";
 
 @Component({
     selector: 'app-file-report-page',
     standalone: true,
     imports: [
       CommonModule,
+      ExamplePdfViewerComponent
     ],
     template: `
     <div class="file-upload-container">
         <div class="top-left-container">
             <h1>Your report is ready!</h1>
-            <h3>Points of focus:</h3>        
-            <ul class="bullet-list">
-                <li class="star">Key terms and obligations</li>
-                <li class="star">Potential risks or red flags</li>
-                <li class="star">Vague or unusual clauses</li>
-                <li class="star">Alignment with standard practices</li>
-                <li class="star">Suggested clarifications or alternatives</li>
-                <li class="star">Sections worth legal review</li>
-            </ul>
+            <div class="bullet-list-container">
+                <div class="bullet-list-title">Points of focus:</div>        
+                <ul class="bullet-list">
+                    <li class="star">Key terms and obligations</li>
+                    <li class="star">Potential risks or red flags</li>
+                    <li class="star">Vague or unusual clauses</li>
+                    <li class="star">Alignment with standard practices</li>
+                    <li class="star">Suggested clarifications or alternatives</li>
+                    <li class="star">Sections worth legal review</li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-right-container">
+            <app-example-pdf-viewer></app-example-pdf-viewer>
         </div>
     </div>`,
     styles: `
@@ -34,6 +41,19 @@ import { Component } from "@angular/core";
         color: var(--primary-grayscale-100);
         margin: 0 !important;
     }
+    .bullet-list-container {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 2rem;
+        
+        .bullet-list-title {
+            font-size: 2rem;
+            font-weight: 500;
+            margin-bottom: 1rem;
+            color: var(--primary-grayscale-100);
+        }
+    }
     .file-upload-container {
         display: flex;
         flex-direction: row;
@@ -41,19 +61,36 @@ import { Component } from "@angular/core";
         width: 100%;
         min-height: calc(100vh - 4rem);
         padding: 2rem;
-    }        
+        
+        @media (max-width: 960px) {
+            flex-direction: column;
+            padding: 0;
+        }
+    }
     .top-left-container {
-        display: flex;
+        flex: 1;
         flex-direction: column;
         gap: 1rem;
-        width: 100%;
-        margin: 32px;
-    }    
+        width: auto;
+        margin: 2rem;
+    }
+    .bottom-right-container {
+        flex: 2;
+        width: auto;
+        height: 100vh;
+        margin: 2rem;
+        padding: 0;
+
+        @media (max-width: 960px) {
+            margin: 0;
+            padding: 0;
+        }
+    }
     .bullet-list {
         color: var(--primary-grayscale-50);
         list-style-type: none;
         padding: 0;
-        margin: 1rem 0 0 2rem;
+        margin-left: 1rem;
         
         li {
             position: relative;
@@ -70,7 +107,8 @@ import { Component } from "@angular/core";
                 font-size: 1.25rem;
             }
         }
-    }`
+    }
+    `
   })
   export class FileReportPageComponent {
 
