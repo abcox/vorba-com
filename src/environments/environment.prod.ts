1acfc707e27ec1003462f3d01d066c193192c9bf
Adam Cox <adam@adamcox.net>
2026-04-12 17:57:35 -0400

    fix(config): use HTTPS ACI file-service endpoint without port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.yourdomain.com/api',  // DEPRECATED
   backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
-  fileServiceApiUrl: 'https://vorba-file-service-4.canadaeast.azurecontainer.io:8080',
+  fileServiceApiUrl: 'https://vorba-file-service-4.canadaeast.azurecontainer.io',
   stripePublicKey: 'pk_live_51SH85qLm8WjfqU8oOlnYytr1tRgEj0Bf6XeuyzzxfTPIE38zJPl10nkKo0qr2XWwS7Yyrjw64zzQqccpHJBqwiy100UtQ8vAXg' // live key
 };
