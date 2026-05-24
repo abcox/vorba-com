1fe26809c2c4ddb19ebd93e373df872c0ec26ce3
Adam Cox <adam@adamcox.net>
2026-05-24 11:47:12 -0400

    refactor(config): add profile social data to environment
    Introduce profile metadata and socialMediaLinks in dev/prod environment files as an interim source before moving this content behind a vorba-service endpoint.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,38 @@ export const environment = {
   backendApiUrl: 'https://demo-app5-api.azurewebsites.net',
   // fileServiceApiUrl: 'https://vorba-file-service-4.canadaeast.azurecontainer.io',
   fileServiceApiUrl: 'https://vorba-file-service-3.azurewebsites.net',
-  stripePublicKey: 'pk_live_51SH85qLm8WjfqU8oOlnYytr1tRgEj0Bf6XeuyzzxfTPIE38zJPl10nkKo0qr2XWwS7Yyrjw64zzQqccpHJBqwiy100UtQ8vAXg' // live key
+  stripePublicKey: 'pk_live_51SH85qLm8WjfqU8oOlnYytr1tRgEj0Bf6XeuyzzxfTPIE38zJPl10nkKo0qr2XWwS7Yyrjw64zzQqccpHJBqwiy100UtQ8vAXg', // live key
+
+/* TODO: Make endpoint in the API that will fetch the personalized data for this site, meanwhile
+   Use this area to make personal object for filling data */
+   profiles: [
+    {
+      name: 'Vorba Corporation',
+      title: 'Innovating the Future of Technology',
+      photoUrl: 'https://media.licdn.com/dms/image/C4D03AQH8n9sXo2l7w/profile-displayphoto-shrink_400_400/0/1517014417693?e=1701302400&v=beta&t=5m1j6h8ZtqjKkJr2a9n1u9sXoQy7z5c5g5g5g5g5g',
+      socialMediaLinks: {
+        github: 'https://github.com/abcox/',
+        linkedin: 'https://www.linkedin.com/company/vorba-corporation',
+        twitter: 'https://twitter.com/vorbacorp',
+        facebook: 'https://www.facebook.com/vorbacorp',
+        instagram: 'https://www.instagram.com/vorbacorp/',
+        tiktok: 'https://www.tiktok.com/@vorbacorp',
+        youtube: 'https://www.youtube.com/@vorbacorp'
+      }
+    },    
+    {
+      name: 'Adam Cox',
+      title: 'Founder & CEO',
+      photoUrl: 'https://media.licdn.com/dms/image/C4D03AQH8n9sXo2l7w/profile-displayphoto-shrink_400_400/0/1517014417693?e=1701302400&v=beta&t=5m1j6h8ZtqjKkJr2a9n1u9sXoQy7z5c5g5g5g5g5g',
+      socialMediaLinks: {
+        github: 'https://github.com/abcox/',
+        linkedin: 'https://www.linkedin.com/in/adamcox27/',
+        twitter: 'https://twitter.com/bainton',
+        facebook: 'https://www.facebook.com/adamcox27',
+        instagram: 'https://www.instagram.com/adamcox27/',
+        tiktok: 'https://www.tiktok.com/@adamcox27',
+        youtube: 'https://www.youtube.com/@adamcox1783'
+      }
+    }
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,38 @@ export const environment = {
   //apiUrl: 'http://localhost:3000/api'  // Adjust this to match your backend URL
   backendApiUrl: 'http://localhost:3000/api',
   fileServiceApiUrl: 'http://localhost:3000',
-  stripePublicKey: 'pk_test_51SH85qLm8WjfqU8oW0UYIndRc4g7mSc5SGjggQfHvCiHxKERIEucNmSCi6nF5O0O3yQYt7ROs38yNhhmNo1im7Hw00dWG5YtyY' // test key
+  stripePublicKey: 'pk_test_51SH85qLm8WjfqU8oW0UYIndRc4g7mSc5SGjggQfHvCiHxKERIEucNmSCi6nF5O0O3yQYt7ROs38yNhhmNo1im7Hw00dWG5YtyY', // test key
+
+/* TODO: Make endpoint in the API that will fetch the personalized data for this site, meanwhile
+   Use this area to make personal object for filling data */
+   profiles: [
+    {
+      name: 'Vorba Corporation',
+      title: 'Innovating the Future of Technology',
+      photoUrl: 'https://media.licdn.com/dms/image/C4D03AQH8n9sXo2l7w/profile-displayphoto-shrink_400_400/0/1517014417693?e=1701302400&v=beta&t=5m1j6h8ZtqjKkJr2a9n1u9sXoQy7z5c5g5g5g5g5g',
+      socialMediaLinks: {
+        github: 'https://github.com/abcox/',
+        linkedin: 'https://www.linkedin.com/company/vorba-corporation',
+        twitter: 'https://twitter.com/vorbacorp',
+        facebook: 'https://www.facebook.com/vorbacorp',
+        instagram: 'https://www.instagram.com/vorbacorp/',
+        tiktok: 'https://www.tiktok.com/@vorbacorp',
+        youtube: 'https://www.youtube.com/@vorbacorp'
+      }
+    },    
+    {
+      name: 'Adam Cox',
+      title: 'Founder & CEO',
+      photoUrl: 'https://media.licdn.com/dms/image/C4D03AQH8n9sXo2l7w/profile-displayphoto-shrink_400_400/0/1517014417693?e=1701302400&v=beta&t=5m1j6h8ZtqjKkJr2a9n1u9sXoQy7z5c5g5g5g5g5g',
+      socialMediaLinks: {
+        github: 'https://github.com/abcox/',
+        linkedin: 'https://www.linkedin.com/in/adamcox27/',
+        twitter: 'https://twitter.com/bainton',
+        facebook: 'https://www.facebook.com/adamcox27',
+        instagram: 'https://www.instagram.com/adamcox27/',
+        tiktok: 'https://www.tiktok.com/@adamcox27',
+        youtube: 'https://www.youtube.com/@adamcox1783'
+      }
+    }
+  ]
 };
