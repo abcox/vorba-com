6b4593d229f1453b1d8dd6d6f358d207bb939b6b
Adam Cox <adam@adamcox.net>
2026-05-27 00:42:31 -0400

    fix(release): point local backend api to vorba-service port 3200

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   name: 'Development (Local)',
   production: false,
   //apiUrl: 'http://localhost:3000/api'  // Adjust this to match your backend URL
-  backendApiUrl: 'http://localhost:3000/api',
+  backendApiUrl: 'http://localhost:3200/api',
   fileServiceApiUrl: 'http://localhost:3000',
   stripePublicKey: 'pk_test_51SH85qLm8WjfqU8oW0UYIndRc4g7mSc5SGjggQfHvCiHxKERIEucNmSCi6nF5O0O3yQYt7ROs38yNhhmNo1im7Hw00dWG5YtyY', // test key
 
